5d62c42aa569e4488b8ac371f9fbefa079f41a65
Davide Merli <davide.merli@outlook.com>
2021-05-22 22:52:04 +0200

    fix: trying to make shader loading work on github pages

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 export async function loadShaders() {
-    const vertexShader = await fetch('/src/shaders/vertexShader.vert');
-    const fragmentShader = await fetch('/src/shaders/fragmentShader.frag');
+    const vertexShader = await fetch('./src/shaders/vertexShader.vert');
+    const fragmentShader = await fetch('./src/shaders/fragmentShader.frag');
 
     const vs = await vertexShader.text();
     const fs = await fragmentShader.text();
